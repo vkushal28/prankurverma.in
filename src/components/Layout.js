967782d942a0843744235a85687e5c4ce98dcf82
kushal <vermakushal.28@gmail.com>
2021-10-21 23:26:39 +0530

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import {useState, useEffect, useRef} from 'react'
 import { Helmet } from 'react-helmet'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
